d682ae880ffcb04f48aea6197035c2c7a7d8da96
Anthony Michelizzi <cynical89@gmail.com>
2016-11-13 12:39:53 -0800

    made login functional

---
+++ javascript/index.js
@@ -0,0 +1,25 @@
+"use strict";
+// const config = require("./config.json");
+
+$("#signin").on("click", (e) => {
+	e.preventDefault();
+	const username = $("#gc_user").val();
+	const password = $("#gc_pass").val();
+	$.ajax({
+		type: "POST",
+		dataType: "json",
+		url: `${config.site.apiHost}:${config.site.port}/login`,
+		data: {username: username, password: password}
+	}).done(function(result) {
+		if (result.error === true) {
+			alert(result.message);
+			return console.error(result.message);
+		}
+		// do something with the success, like show a link
+		console.log(result);
+	}).fail(function(err) {
+		// do something with the failure, like laugh at the user
+		window.alert("hahahahaha! NO!");
+		console.error(err);
+	});
+})
